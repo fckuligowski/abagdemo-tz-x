4ed7b8b53bddbeba2659bde400f4cdfb4ee84a4e
fckuligowski <frank.kuligowski@thinkahead.com>
2020-06-04 20:07:33 +0000

    add /scan POST

--- project/bags/routes.py
+++ project/bags/routes.py
@@ -1,5 +1,5 @@
 from . import bags_blueprint
-from flask import current_app
+from flask import current_app, request
 import flask
 import json
 from google.cloud import storage
@@ -8,7 +8,18 @@ from google.cloud.storage import Blob
 @bags_blueprint.route('/')
 @bags_blueprint.route('/index')
 def index():
-    return 'Welcome to the %s application.' % current_app.config.get('APP_NAME')    
+    return 'Welcome to the %s application.' % current_app.config.get('APP_NAME')
+
+@bags_blueprint.route('/scan', methods=['POST'])
+def scan():
+    rtn = {
+        'scan': ''
+    }
+    if request.method == 'POST':
+        # scan = json.loads(request.get_json())
+        scan = request.get_json()
+        rtn['scan'] = save_bag_scan(scan)
+    return rtn
 
 @bags_blueprint.route('/status')
 @bags_blueprint.route('/status/<bag_id>')
@@ -57,3 +68,20 @@ def get_bag_data(bag_id):
         if data_item['bag'] == bag_id:
             rtn.append(data_item)
     return rtn
+
+def save_bag_scan(scan):
+    # Read in the file from GCP storage
+    storage_client = storage.Client()
+    bucket_name = current_app.config.get('DATA_BUCKET_NAME')
+    bucket = storage_client.get_bucket(bucket_name)
+    fname = current_app.config.get('DATA_FILE_NAME')
+    blob = Blob(fname, bucket)
+    data_str = blob.download_as_string()
+    data_dict = json.loads(data_str)
+    # Add record to end of data
+    data_dict.append(scan)
+    print(json.dumps(data_dict, indent=4))
+    # Save the data back to GCP storage
+    blob.upload_from_string(json.dumps(data_dict, indent=4))
+    return len(data_dict)
+
