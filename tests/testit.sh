a061b277280717c028e63016c72eabcef5e59e59
fckuligowski <frank.kuligowski@thinkahead.com>
2020-06-10 16:04:56 +0000

    test reports

--- tests/testit.sh
+++ tests/testit.sh
@@ -1,5 +1,5 @@
 # Run the tests using pytest
 export PYTHONPATH=`pwd`
-pytest tests/$1 --setup-show $2 $3 $4 $5 $6 $7 $8 $9
+pytest tests/$1 -v $2 $3 $4 $5 $6 $7 $8 $9
 # OR for less verbose output
 # pytest -v
