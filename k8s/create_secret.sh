0aa92439cb2db815546fdcec2056c3489760fd39
Ubuntu <ubuntu@ip-172-31-54-185.ec2.internal>
2020-08-12 21:58:12 +0000

    add log sinks

--- k8s/create_secret.sh
+++ k8s/create_secret.sh
@@ -1,3 +1,3 @@
 kubectl create secret generic abagdemo-gcp-creds \
---from-file=./instance/creds/hazel-math-279814.json -n abagdemo \
+--from-file=../instance/creds/hazel-math-279814.json -n abagdemo \
 --dry-run -o yaml | kubectl apply -f -
